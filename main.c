3e5f312b61343fad1f14ef547808c14745aea793
tictac22 <fominsartems947@gmail.com>
2025-05-31 21:56:29 +0300

    some progress

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 
 
 #include "include/raylib.h"
-
+#include <stdio.h>
 const int screenWidth = 1200;
 const int screenHeight = 600;
 
+int player_left_score = 0;
+int player_right_score = 0;
 
 typedef struct Circle
 {
@@ -13,7 +15,11 @@ typedef struct Circle
     int radius;
 } Circle;
 
-
+typedef struct Paddle
+{
+    Rectangle rectangle;
+    float delta;
+} Paddle; 
 
 void update_circle_coordinates(Circle *circle)
 {
@@ -21,23 +27,67 @@ void update_circle_coordinates(Circle *circle)
     circle->position.x += circle->delta.x;
     circle->position.y += circle->delta.y;
 
-    if(circle->position.x + circle->radius >= screenWidth || circle->position.x - circle->radius <= 0) 
-    {
-        circle->delta.x *= -1;
-    }
-
     if(circle->position.y + circle->radius >= screenHeight || circle->position.y - circle->radius <= 0)
     {
         circle->delta.y *= -1;
     }
 }
 
+
+void update_paddles_coordinate(Paddle *left_paddle, Paddle *right_paddle)
+{
+
+    if(IsKeyDown(KEY_W) && left_paddle->rectangle.y - left_paddle->delta >= 0)
+    {
+        left_paddle->rectangle.y -= left_paddle->delta;
+    }
+
+        if(IsKeyDown(KEY_S) && left_paddle->rectangle.y + left_paddle->rectangle.height + left_paddle->delta <= screenHeight)
+        {
+        left_paddle->rectangle.y += left_paddle->delta;
+    }
+
+    if(IsKeyDown(KEY_UP) && right_paddle->rectangle.y - right_paddle->delta >= 0)
+    {
+        right_paddle->rectangle.y -= right_paddle->delta;
+    }
+
+    if(IsKeyDown(KEY_DOWN) && right_paddle->rectangle.y + right_paddle->rectangle.height + right_paddle->delta <= screenHeight)
+    {
+        right_paddle->rectangle.y += right_paddle->delta;
+    }
+}
+
+void check_colision(Paddle *paddle, Circle *circle)
+{
+    
+    int paddle_left = paddle->rectangle.x;
+    int paddle_right = paddle->rectangle.x + paddle->rectangle.width;
+
+    int paddle_top = paddle->rectangle.y;
+    int paddle_bottom = paddle->rectangle.y + paddle->rectangle.height;
+
+
+
+    int circle_left = circle->position.x - circle->radius;
+    int circle_top = circle->position.y - circle->radius;
+    int circle_right = circle->position.x + circle->radius;
+    int circle_bottom = circle->position.y + circle->radius;
+
+    if(circle_right >= paddle_left && circle_left <= paddle_right && circle_top <= paddle_bottom && circle_bottom >= paddle_top)
+    {
+        circle->delta.x *= -1;
+    }
+
+}
+
 int main(void)
 {
 
+    int circle_speed = 10;
     Circle circle = {
         { screenWidth / 2 ,screenHeight / 2},
-        {4,4},
+        {-circle_speed,-circle_speed},
         10,
     };
 
@@ -46,21 +96,25 @@ int main(void)
     int paddle_height = 150;
     int paddle_x_position = 25;
     int paddle_y_position = screenHeight / 2  - (paddle_height / 2);
-    Rectangle left_paddle = 
-    {
-        paddle_x_position,
-        paddle_y_position,
-        paddle_width,
-        paddle_height,
+    Paddle left_paddle = {
+        {
+            paddle_x_position,
+            paddle_y_position,
+            paddle_width,
+            paddle_height,
+        },
+        5
     };
 
     
-    Rectangle right_paddle = 
-    {
-        screenWidth - paddle_x_position - paddle_width,
-        paddle_y_position,
-        paddle_width,
-        paddle_height
+    Paddle right_paddle = {
+        {
+            screenWidth - paddle_x_position - paddle_width,
+            paddle_y_position,
+            paddle_width,
+            paddle_height
+        },
+        5
     };
 
     InitWindow(screenWidth, screenHeight, "raylib pong game");
@@ -70,13 +124,20 @@ int main(void)
     while (!WindowShouldClose())        
     {
 
-        ClearBackground(BLACK);
         update_circle_coordinates(&circle);
+        update_paddles_coordinate(&left_paddle, &right_paddle);
+        check_colision(&left_paddle, &circle);
+        check_colision(&right_paddle, &circle);
         BeginDrawing();
-        DrawCircleV(circle.position, circle.radius, PINK);
+            ClearBackground(BLACK);
+            DrawCircleV(circle.position, circle.radius, PINK);
 
-        DrawRectangleRec(left_paddle, WHITE);
-        DrawRectangleRec(right_paddle, WHITE);
+            DrawRectangleRec(left_paddle.rectangle, WHITE);
+            DrawRectangleRec(right_paddle.rectangle, WHITE);
+
+            DrawText(TextFormat("%i", player_left_score),  screenWidth / 2  - 100, 20, 32, WHITE );
+
+            DrawText(TextFormat("%i", player_right_score),  screenWidth / 2  + 100, 20, 32, WHITE );
         EndDrawing();
     }
 
